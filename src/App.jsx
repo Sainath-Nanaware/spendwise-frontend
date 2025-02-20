2c6cb7c0dd17b822f2d2510b68006f5bf35a023b
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-02-20 15:25:16 +0530

    project setup with redux, tailwindcss and router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
       <h1>Ganpati Bappa Morya</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+
     </>
   )
 }
