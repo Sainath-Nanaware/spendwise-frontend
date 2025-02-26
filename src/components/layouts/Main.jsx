344c1b50e2b477664c30e1513cf0ea6e69c38e69
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-02-26 20:29:34 +0530

    apply styles on main containt

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
-import ladyImage from '../../../public/images/AccountingWoman.jpg'
+import mainImage from '../../../public/images/AccountingWoman.jpg'
 function Main() {
   return (
-    <main className='pb h-[80vh] flex justify-center items-center pl-[5vw] pr-[5vw]'>
+    <main className=' h-[20vh] flex  flex-col justify-center items-center md:h-[80vh] md:flex-row md:justify-center md:items-center md:pl-[5vw] md:pr-[5vw]'>
         {/* Text Area */}
-        <div className='pb h-[100%] w-[50%] flex flex-col justify-center items-center'>
+        <div className='  h-[100%] w-[50%] flex flex-col justify-start items-center gap-[6vh] pt-[18vh]'>
             {/* Tag Line  */}
-            <p className='pb font-bold text-5xl'>
-                 Track smart,spend wiser 
+            <div className=' md:pl-[12px] md:w-[100%]'>
+            <p className='  font-bold text-5xl md:text-6xl text-[#333]'>
+                 Track smart, 
             </p>
+            <p className=' font-bold text-6xl pl-[10vw] text-[#4CAF50]'>spend wiser</p>
+
+            </div>
            
            {/* tag line description */}
-            <p>"Take control of your finances effortlessly with SpendWise. Our smart tracking tools help you visualize your expenses, manage your budget, and build a brighter financial future. Make every purchase a wise decision—track smart, spend wiser!"</p>
+           <div className=' pl-[4vw] w-[100%]'>
+            <p className=' w-[40vw] p-[12px] rounded-xl bg-[#EEEEEE] text-[#625959] text-start text-[17px] font-semibold '>"Let’s face it – keeping track of expenses can be overwhelming. But what if it didn’t have to be? With our easy-to-use tools, you can <span className='font-bold text-[#4CAF50]' >track your spending smartly</span>  and <span className='font-bold text-[#4CAF50]'>make wiser financial choices</span> without breaking a sweat."</p>
+
+           </div>
         </div>
         {/* Image Area */}
-        <div className='pb h-[100%] w-[50%] flex justify-center items-center '>
-            <img src={ladyImage} alt="ladyImage"  className='h-[70vh]'/>
+        <div className='  h-[100%] w-[50%] flex justify-center items-center '>
+            <img src={mainImage} alt="ladyImage"  className=' h-[74vh] rounded-2xl'/>
         </div>
 
     </main>
